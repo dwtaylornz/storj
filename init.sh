28d673ab7ccfd7e5c16e1432651b0869966dcf62
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-01-26 10:43:12 +1300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+# start daemon and instances
+# loop status with colours removed from output 
+
+storjshare daemon
+storjshare start --config /config/config-1
+storjshare start --config /config/config-2
+storjshare start --config /config/config-3
+storjshare start --config /config/config-4
+storjshare start --config /config/config-5
+
+sleep 60
+storjshare status | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g"
+
+# loop status every hour;
+
+while :
+do
+
+        sleep 3600
+        storjshare status | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g"
+
+done
